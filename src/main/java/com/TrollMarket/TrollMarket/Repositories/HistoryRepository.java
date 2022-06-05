da73aa4a605b10e1eb026f2058ace6e834e167e3
Ahmad Shobari <ahmadasob54@gmail.com>
2022-06-05 15:59:33 +0700

    HistoryRepository added

---
+++ src/main/java/com/TrollMarket/TrollMarket/Repositories/HistoryRepository.java
@@ -0,0 +1,7 @@
+package com.TrollMarket.TrollMarket.Repositories;
+
+import com.TrollMarket.TrollMarket.Models.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoryRepository extends JpaRepository<History, Long> {
+}
